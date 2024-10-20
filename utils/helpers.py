8beaa0915a820e5680ce5da83ccf56e63b897e3b
nitza <nitzan.manor@campus.technion.ac.il>
2024-10-20 17:18:53 +0300

    Styles

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,4 +1,4 @@
-STYLES_LIST = ['friendly', 'formal', 'neutral']
+STYLES_LIST = ['kids', 'elderly', 'emoji', 'rhymes']
 
 
 def get_style_instructions(style: str) -> str:
@@ -15,10 +15,22 @@ def get_style_instructions(style: str) -> str:
     else:
         if style not in STYLES_LIST:
             raise ValueError(f'Style must be in {STYLES_LIST}')
-        if style == 'formal':
-            return 'write in a formal style'
-        elif style == 'friendly':
-            return 'write in a friendly style'
-        elif style == 'neutral':
-            return 'write in a neutral style'
+        if style == 'kids':
+            return ('When responding, simplify complex concepts into easy-to-understand language.'
+                    'Use analogies and relatable examples, keeping explanations brief and straightforward.'
+                    'Avoid discussing rough topics, such as genitals or abortion,'
+                    'that may not be suitable for a child’s understanding.')
+        elif style == 'elderly':
+            return ('When addressing questions from elderly users, adopt a formal and respectful tone.'
+                    'Provide clear, concise explanations to ensure understanding, especially for complex topics.'
+                    'Use phrases like Let me clarify this for you or It\'s important to know that...'
+                    'to guide them through the information.')
+        elif style == 'emoji':
+            return ('When responding to younger users, adopt a friendly and engaging tone.'
+                    'Use clear explanations while incorporating some light slang to create a relatable vibe.'
+                    'Use emojis to enhance engagement and express warmth, fun and approachable.')
+        elif style == 'rhymes':
+            return ('When responding, use rhymes to make the conversation fun and engaging.'
+                    'Create playful and light-hearted responses to keep the user entertained.'
+                    'Use simple rhyming words to make the conversation easy to follow and enjoyable.')
 
